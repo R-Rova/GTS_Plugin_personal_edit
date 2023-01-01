80f8d003c543a5925c727e7d4c7e6b5042638acb
R-Rova <121738070+R-Rova@users.noreply.github.com>
2023-01-01 02:39:50 +0100

    Update GtsSizeManager.cpp

--- src/managers/GtsSizeManager.cpp
+++ src/managers/GtsSizeManager.cpp
@@ -27,25 +27,38 @@ namespace Gts {
 		return GetNode ? NodeScale : 1; // <- not used, causes troubles with quest progression. (Can't reach 1.44 for example when 1.50 is needed.)
 	}
 
+	std::string SizeManager::DebugName() {
+		return "SizeManager";
+	}
+
 	void SizeManager::Update() {
 		for (auto actor: find_actors()) {
 
-			bool Balance = false; // Toggles Balance Mode for the mod. False = off, true = on. 
+			bool Balance = false; // Toggles Balance Mode for the mod. False = off, true = on.
 
-			auto& runtime = Runtime::GetSingleton();
+			float QuestStage = Runtime::GetStage("MainQuest");
+
 			float Gigantism = this->GetEnchantmentBonus(actor)/100;
-			float GetLimit = clamp(1.0, 99999999.0, runtime.sizeLimit->value);
+			float GetLimit = clamp(1.0, 99999999.0, Runtime::GetFloat("sizeLimit")); // Default size limit
 			float Persistent_Size = Persistent::GetSingleton().GetData(actor)->bonus_max_size;
-			float SelectedFormula = runtime.SelectedSizeFormula->value;
+			float SelectedFormula = Runtime::GetInt("SelectedSizeFormula");
 
-			//BalancedMode(Balance);
+			float FollowerLimit = Runtime::GetFloat("FollowersSizeLimit");
+			float NPCLimit = Runtime::GetFloat("NPCSizeLimit");
 
-			if (SelectedFormula >= 2.0) {
-				GetLimit = runtime.MassBasedSizeLimit->value +1.0;
+			if (SelectedFormula >= 2.0 && actor->formID == 0x14) { // Apply Player Mass-Based max size
+				GetLimit = clamp(1.0, 99999999.0, Runtime::GetFloat("MassBasedSizeLimit"));
 			}
+			else if (QuestStage > 100 && FollowerLimit > 1 && actor->formID != 0x14 && (Runtime::InFaction(actor, "FollowerFaction") || actor->IsPlayerTeammate())) { // Apply Follower Max Size
+					GetLimit = clamp(1.0, 99999999.0, Runtime::GetFloat("FollowersSizeLimit")); // Apply only if Quest is done.
+			}
+			else if (QuestStage > 100 && NPCLimit > 1 &&  actor->formID != 0x14 && (!Runtime::InFaction(actor, "FollowerFaction") && !actor->IsPlayerTeammate())) {  // Apply Other NPC's max size
+					GetLimit = clamp(1.0, 99999999.0, Runtime::GetFloat("NPCSizeLimit")); 		// Apply only if Quest is done.
+			}
+
 			float RaceScale = (GetRaceScale(actor) * (GetLimit + Persistent_Size)) * (1.0 + Gigantism);
 			float TotalLimit = (GetLimit + Persistent_Size) * (1.0 + Gigantism);
-			
+
 			if (TotalLimit < 1.0) {
 				TotalLimit = 1.0;
 			}
@@ -68,7 +81,7 @@ namespace Gts {
 		this->GetData(actor).enchantmentBonus += amt;
 	}
 
-//=================Size Hunger
+	//=================Size Hunger
 
 	void SizeManager::SetSizeHungerBonus(Actor* actor, float amt) {
 		this->GetData(actor).SizeHungerBonus = amt;
@@ -83,7 +96,7 @@ namespace Gts {
 		this->GetData(actor).SizeHungerBonus += amt;
 	}
 
-//==================Growth Spurt
+	//==================Growth Spurt
 
 	void SizeManager::SetGrowthSpurt(Actor* actor, float amt) {
 		this->GetData(actor).GrowthSpurt = amt;
@@ -98,18 +111,87 @@ namespace Gts {
 		this->GetData(actor).GrowthSpurt += amt;
 	}
 
-//===============Balance Mode 
+	//================Size-Related Damage
+	void SizeManager::SetSizeAttribute(Actor* actor, float amt, float attribute) {
+		auto Persistent = Persistent::GetSingleton().GetData(actor);
+		if (attribute == 0) {
+			Persistent->NormalDamage = amt;
+		}
+		else if (attribute == 1) {
+			Persistent->SprintDamage = amt;
+		}
+		else if (attribute == 2) {
+			Persistent->FallDamage = amt;
+		} 
+		else if (attribute == 3) {
+			Persistent->HHDamage = amt;
+		}
+	}
+
+	float SizeManager::GetSizeAttribute(Actor* actor, float attribute) {
+		auto Persistent = Persistent::GetSingleton().GetData(actor);
+		float Normal = clamp (1.0, 999999.0, Persistent->NormalDamage);
+		float Sprint = clamp (1.0, 999999.0, Persistent->SprintDamage);
+		float Fall = clamp (1.0, 999999.0, Persistent->FallDamage);
+		float HH = clamp (1.0, 999999.0, Persistent->HHDamage);
+		if (attribute == 0) {
+			return Normal;
+		}
+		else if (attribute == 1) {
+			return Sprint;
+		}
+		else if (attribute == 2) {
+			return Fall;
+		} 
+		else if (attribute == 3) {
+			return HH;
+		}
+		return 1.0;
+	}
+
+	void SizeManager::ModSizeAttribute(Actor* actor, float amt, float attribute) {
+		auto Persistent = Persistent::GetSingleton().GetData(actor);
+		if (attribute == 0) {
+			Persistent->NormalDamage += amt;
+		}
+		else if (attribute == 1) {
+			Persistent->SprintDamage += amt;
+		}
+		else if (attribute == 2) {
+			Persistent->FallDamage += amt;
+		} 
+		else if (attribute == 3) {
+			Persistent->HHDamage += amt;
+		}
+	}
+    //===============Size-Related Attribute End
+
+
+	//===============Size-Vulnerability
+
+	void SizeManager::SetSizeVulnerability(Actor* actor, float amt) {
+		Persistent::GetSingleton().GetData(actor)->SizeVulnerability = amt;
+	}
+
+	float SizeManager::GetSizeVulnerability(Actor* actor) {
+		return clamp (0.0, 999999.0, Persistent::GetSingleton().GetData(actor)->SizeVulnerability);
+	}
+
+	void SizeManager::ModSizeVulnerability(Actor* actor, float amt) {
+		Persistent::GetSingleton().GetData(actor)->SizeVulnerability += amt;
+	}
+    //===============Size-Vulnerability
+
+	//===============Balance Mode
 	float SizeManager::BalancedMode()
 	{
-		if (Runtime::GetSingleton().BalanceMode->value >= 1.0) {
+		if (Runtime::GetBool("BalanceMode")) {
 			//log::info("Balance Mode True");
 			return 2.0;
-		}
-		else if (Runtime::GetSingleton().BalanceMode->value < 1.0) {
+		} else {
 			//log::info("Balance Mode False");
 			return 1.0;
 		}
-		return 1.0;
 	}
 
 	SizeManagerData& SizeManager::GetData(Actor* actor) {
@@ -118,9 +200,9 @@ namespace Gts {
 	}
 
 	void SizeManager::Reset() {
-			auto caster = PlayerCharacter::GetSingleton();
-			SetEnchantmentBonus(caster, 0.0);
-			SetGrowthSpurt(caster, 0.0);
-			//log::info("Resetting Enchantment and Growth Spurt");
-		}
+		auto caster = PlayerCharacter::GetSingleton();
+		SetEnchantmentBonus(caster, 0.0);
+		SetGrowthSpurt(caster, 0.0);
+		//log::info("Resetting Enchantment and Growth Spurt");
+	}
 }
