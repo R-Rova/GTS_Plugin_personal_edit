73aedd8ed8fcd17fe819341a16bfc4857647b804
R-Rova <121738070+R-Rova@users.noreply.github.com>
2023-04-12 21:46:56 +0200

    Edit pos or neg?

--- src/managers/RandomGrowth.cpp
+++ src/managers/RandomGrowth.cpp
@@ -116,10 +116,12 @@ namespace Gts {
 		} else if (this->AllowGrowth == true && !hasSMT) {
 			// Do the growing
 			float delta_time = Time::WorldTimeDelta();
-			int TotalPower = (100 + this->Randomize)/100;
+			int rnd = rand();
+			int PosOrneg = (rnd & 1) ? 1 : -1; // Roll for positive or negative gain in size
+			int TotalPower = ((100 + this->Randomize)/100) * PosOrneg;
 			float Scale = get_target_scale(player);
 			float ProgressionMultiplier = Runtime::GetFloatOr("ProgressionMultiplier", 1.0);
-			float base_power = ((0.00185 * TotalPower * 60.0 * Scale) * ProgressionMultiplier);  // Put in actual power please
+			float base_power = ((0.0.0025 * TotalPower * 60.0 * Scale) * ProgressionMultiplier);  // Put in actual power please
 			RestoreStats(); // Regens Attributes if PC has perk
 			mod_target_scale(player, base_power * delta_time); // Use delta_time so that the growth will be the same regardless of fps
 			GrowthTremorManager::GetSingleton().CallRumble(player, player, base_power * 180);
